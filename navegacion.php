861805d2ab84f6a720ba583d1e9556508aea3791
Javi Martínez <55922195+Javitoox@users.noreply.github.com>
2020-04-14 02:51:05 +0200

    Barra de navegación en móvil(queda en ipad y escritorio)

--- navegacion.php
+++ navegacion.php
@@ -1,3 +1,31 @@
-<?php
-    phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Inicio</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	<link rel="shortcut icon" href="images/logo.png">
+	<link rel="apple-touch-icon" href="images/logo.png">
+	<link rel="stylesheet" type="text/css" href="css/navegacion.css">
+</head>
+<body>
+	<?php include_once("fondo.php"); ?>
+
+	<ul>
+		<div class="caja"><li><a href="navegacion.php">
+			<img id="esquina" src="images/logo.png" alt="logo esquina">
+		</a></li></div>
+		<div class="caja"><li><a href="navegacion.php">Jugadores</a></li></div>
+		<div class="caja"><li><a href="navegacion.php">Resultados</a></li></div>
+		<div class="caja"><li><a href="navegacion.php">Mis Seguimientos</a></li></div>
+		<div class="caja"><li><a href="navegacion.php">Sobre Nosotros</a></li></div>
+		<div class="caja"><li><a href="navegacion.php">Perfil</a></li></div>
+	</ul>
+	<div id="enlaces">
+		<a href="navegacion.php"><img class="enl" src="images/insta.png" alt="logo"></a>
+		<a href="navegacion.php"><img class="enl" src="images/twich.png" alt="logo"></a>
+		<a href="navegacion.php"><img class="enl" src="images/twitter.png" alt="logo"></a>
+	</div>
+	
+</body>
+</html>
