1a62523a1ec6b82ed411b2e8c6b017db9f53cd9d
mrb <62081471+maribelrb22@users.noreply.github.com>
2020-04-24 21:09:26 +0200

    Después de 500000 horas más tardes por fin me sale! Menú de navegación hecho para escritorio y menú desplegable para ipad móvil. He dejado algunos comentarios, si no entendéis algo ya me decías;)

--- navegacion.php
+++ navegacion.php
@@ -8,24 +8,48 @@
 	<link rel="apple-touch-icon" href="images/logo.png">
 	<link rel="stylesheet" type="text/css" href="css/navegacion.css">
 </head>
+	
 <body>
 	<?php include_once("fondo.php"); ?>
 
-	<ul>
-		<div class="caja caja2"><li><a href="navegacion.php">
-			<img id="esquina" src="images/logo.png" alt="logo esquina">
-		</a></li></div>
-		<div class="caja"><li><a href="navegacion.php">Jugadores</a></li></div>
-		<div class="caja"><li><a href="navegacion.php">Resultados</a></li></div>
-		<div class="caja"><li><a href="navegacion.php">Mis Seguimientos</a></li></div>
-		<div class="caja"><li><a href="navegacion.php">Sobre Nosotros</a></li></div>
-		<div class="caja"><li><a href="navegacion.php">Perfil</a></li></div>
-	</ul>
-	<div id="enlaces">
-		<a href="https://www.instagram.com/udcesports/"><img class="enl" src="images/insta.png" alt="logo"></a>
-		<a href="https://www.twitch.tv/udconstantinaesports/"><img class="enl" src="images/twich.png" alt="logo"></a>
-		<a href="https://twitter.com/udcesports?lang=es"><img class="enl" src="images/twitter.png" alt="logo"></a>
-	</div>
+	<div class="topnav" id="myTopnav">
+		
+		<a href="navegacion.php"><img id="imglogo" src="images/logo.png" alt="Logo"></a>
+		<br><a href="navegacion.php">Jugadores</a>
+		<a href="navegacion.php">Resultados</a>
+		<a href="navegacion.php">Mis Seguimientos</a>
+		<a href="navegacion.php">Sobre Nosotros</a>
+		<a href="navegacion.php">Perfil</a>
+
+		<div class="dropdown">
+		  <button class="dropbtn">Nuestras Redes 
+		  </button>
+		  <div class="dropdown-content">
+			<a href="https://www.instagram.com/udcesports/" target="_blank">
+			<img id="imgIg" src="images/insta.png">&nbsp Instagram</a>
+			<a href="https://www.twitch.tv/udconstantinaesports/" target="_blank">
+			<img id="imgTwitch" src="images/twich.png">&nbsp Twitch</a>
+			<a href="https://twitter.com/udcesports?lang=es" target="_blank">
+			<img id="imgTwitter" src="images/twitter.png">&nbsp Twitter</a>
+		  </div>
+		</div> 
+		<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+	  </div>
+	  
+	  <script>
 	
+	/*Lo que hace es agregar o no la clase "responsive". Se activará si se hace clic en 
+	el icono del menú (topnav) que, estará disponible para móviles.*/ 
+
+	  function myFunction() {
+		var x = document.getElementById("myTopnav");
+		if (x.className === "topnav") {
+		  x.className += " responsive";
+		} else {
+		  x.className = "topnav";
+		}
+	  }
+	  </script>
+
 </body>
 </html>
