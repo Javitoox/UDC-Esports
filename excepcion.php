4e8e015d8514cf62132c2fd7aeadce98c22778c7
mrb <62081471+maribelrb22@users.noreply.github.com>
2020-04-27 14:31:28 +0200

    He creado headComun.php que contiene los head que son comunes para todas las vistas. En los head de cada vista estará solo los include y los stylesheet de cada una.

--- excepcion.php
+++ excepcion.php
@@ -16,11 +16,8 @@
 <html lang="es">
 <head>
 <title>Excepcion</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <meta name="viewport" content="width=device-width; initial-scale=1.0">
-  <link rel="shortcut icon" href="images/logo.png">
-  <link rel="apple-touch-icon" href="images/logo.png">
-  <link rel="stylesheet" type="text/css" href="css/excepcion.css" />
+	<?php include_once("headComun.php"); ?>
+  	<link rel="stylesheet" type="text/css" href="css/excepcion.css" />
 </head>
 <body>	
 	
--- navegacion.php
+++ navegacion.php
@@ -2,10 +2,7 @@
 <html lang="es">
 <head>
 <title>Inicio</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta name="viewport" content="width=device-width; initial-scale=1.0">
-	<link rel="shortcut icon" href="images/logo.png">
-	<link rel="apple-touch-icon" href="images/logo.png">
+	<?php include_once("headComun.php"); ?>
 	<link rel="stylesheet" type="text/css" href="css/navegacion.css">
 </head>
 	
