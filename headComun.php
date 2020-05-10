299944b0479b976f16ba7a241bae7f25d65a2773
mrb <62081471+maribelrb22@users.noreply.github.com>
2020-05-10 17:51:14 +0200

    Los voy a volver a subir porque no se que ha pasado. headComun.

---
+++ headComun.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Comun</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+    <link rel="shortcut icon" href="images/logo.png">
+    <link rel="apple-touch-icon" href="images/logo.png">
+	
+</head>
+</html>
